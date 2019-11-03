fafa37de94ef1a891837d264834825ed6a76edfe
WeirdFish09 <kovtkoyura@gmail.com>
2019-11-03 14:56:14 +0200

    initial commit

---
+++ NetReaders-Service/src/main/java/com/netreaders/NetReadersApplication.java
@@ -0,0 +1,13 @@
+package com.netreaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetReadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetReadersApplication.class, args);
+	}
+
+}
