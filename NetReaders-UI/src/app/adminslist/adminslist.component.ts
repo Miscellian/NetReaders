d7aa1375256eee10d5cda97b911c615a202c0806
Александра Борсук <sanechkaborsuk@gmail.com>
2019-11-10 18:46:21 +0200

    clientside of userpage (super admin)

---
+++ NetReaders-UI/src/app/adminslist/adminslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminslist',
+  templateUrl: './adminslist.component.html',
+  styleUrls: ['./adminslist.component.css']
+})
+export class AdminslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- NetReaders-UI/src/app/app.module.ts
+++ NetReaders-UI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserpageComponent } from './userpage/userpage.component';
+import { AdminslistComponent } from './adminslist/adminslist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserpageComponent,
+    AdminslistComponent
   ],
   imports: [
     BrowserModule,
