25e40a73bb8f621796a935c73fad5e0958757498
Sanverik <sanverik@gmail.com>
2019-11-21 18:04:11 +0200

    allowed cors

--- NetReaders-Service/src/main/java/com/netreaders/config/SecurityConfig.java
+++ NetReaders-Service/src/main/java/com/netreaders/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable().
+		http.csrf().disable().
         authorizeRequests()
         .antMatchers("/api/users/test").hasAuthority("ADMIN")
         .anyRequest().permitAll()
