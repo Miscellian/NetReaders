8b4226a5b4c5038341b9000d3cfe8dbb1d9c23bf
Sanverik <sanverik@gmail.com>
2019-11-28 15:05:15 +0200

    Fix dao`s exception

--- NetReaders-Service/src/main/java/com/netreaders/dao/genres/GenreDaoImpl.java
+++ NetReaders-Service/src/main/java/com/netreaders/dao/genres/GenreDaoImpl.java
@@ -1,39 +1,40 @@
 package com.netreaders.dao.genres;
 
+import com.netreaders.exception.classes.DataBaseSQLException;
+import com.netreaders.exception.classes.NoSuchModelException;
 import com.netreaders.models.Genre;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Types;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+
+@Repository
 @Log4j
 @PropertySource("classpath:query.properties")
-@Repository
+@AllArgsConstructor
 public class GenreDaoImpl implements GenreDao {
 
-    @Autowired
-    private JdbcTemplate template;
-
-    @Autowired
-    private Environment env;
-
-    @Autowired
-    private GenreMapper genreMapper;
+    private final JdbcTemplate template;
+    private final Environment env;
+    private final GenreMapper genreMapper;
 
     @Override
-    public Genre create(Genre genre) throws SQLException {
+    public Genre create(Genre genre) {
 
         final String sql_query = env.getProperty("genre.create");
 
@@ -42,49 +43,44 @@ public class GenreDaoImpl implements GenreDao {
         // save object into DB and return auto generated PK via KeyHolder
         // or throws DuplicateKeyException if record exist in table
         try {
-            template.update(connection -> {
-                PreparedStatement ps = connection.prepareStatement(sql_query, Statement.RETURN_GENERATED_KEYS);
-                ps.setString(1, genre.getName());
-                return ps;
-            }, holder);
+            template.update(creator(sql_query, genre), holder);
 
-            Integer newId;
-            if (holder.getKeys().size() > 1) {
-                newId = (Integer) holder.getKeys().get("genre_id");
-            } else {
-                newId = holder.getKey().intValue();
-            }
-            genre.setId(newId);
-            log.debug(String.format("Create a new genre with id '%s'", newId));
+            genre.setId(retrieveId(holder));
 
+            log.debug(String.format("Create a new genre with id '%s'", genre.getId()));
             return genre;
 
         } catch (DuplicateKeyException e) {
             log.error(String.format("Genre '%s' is already exist", genre.getName()));
-            throw new SQLException("Internal sql exception");
+            throw new DataBaseSQLException(String.format("Genre '%s' is already exist", genre.getName()));
+        } catch (SQLException e) {
+            log.error("Genre creation fail!");
+            throw new DataBaseSQLException("Genre creation fail!");
         }
     }
 
-    public Genre getById(Integer id) throws SQLException {
+    public Genre getById(Integer id) {
 
         String sql_query = env.getProperty("genre.read");
 
         List<Genre> genres = template.query(sql_query, genreMapper, id);
 
+        checkIfCollectionIsNull(genres);
+
         if (genres.isEmpty()) {
-            log.debug(String.format("Dont find any genre by id '%s'", id));
-            return null;
+            log.error(String.format("Dont find any genre by id '%s'", id));
+            throw new NoSuchModelException(String.format("Dont find any genre by id '%s'", id));
         } else if (genres.size() == 1) {
             log.debug(String.format("Find a genre by id '%s'", id));
             return genres.get(0);
         } else {
             log.error(String.format("Find more than one genre by id '%s'", id));
-            throw new SQLException("Internal sql exception");
+            throw new DataBaseSQLException(String.format("Find more than one genre by id '%s'", id));
         }
     }
 
     @Override
-    public void update(Genre genre) throws SQLException {
+    public void update(Genre genre) {
 
         String sql_query = env.getProperty("genre.update");
         long id = genre.getId();
@@ -97,12 +93,12 @@ public class GenreDaoImpl implements GenreDao {
             log.debug(String.format("Update genre by id '%d'", id));
         } else {
             log.error(String.format("Update more than one genre by id '%d'", id));
-            throw new SQLException("Internal sql exception");
+            throw new DataBaseSQLException(String.format("Update more than one genre by id '%d'", id));
         }
     }
 
     @Override
-    public void delete(Genre genre) throws SQLException {
+    public void delete(Genre genre) {
 
         String sql_query = env.getProperty("genre.delete");
 
@@ -114,16 +110,19 @@ public class GenreDaoImpl implements GenreDao {
             log.debug(String.format("Delete genre by id '%d'", id));
         } else {
             log.error(String.format("Delete more than one genre by id '%d'", id));
-            throw new SQLException("Internal sql exception");
+            throw new DataBaseSQLException(String.format("Delete more than one genre by id '%d'", id));
         }
 
     }
 
-    public Collection<Genre> getAll() throws SQLException {
+    public Collection<Genre> getAll() {
 
         String sql_query = env.getProperty("genre.readAll");
 
         List<Genre> genres = template.query(sql_query, genreMapper);
+
+        checkIfCollectionIsNull(genres);
+
         if (genres.isEmpty()) {
             log.debug("Dont find any genre");
             return Collections.emptyList();
@@ -133,17 +132,46 @@ public class GenreDaoImpl implements GenreDao {
         }
     }
 
-    public Collection<Genre> getByBookId(int id) throws SQLException {
+    public Collection<Genre> getByBookId(int id) {
 
         String sql_query = env.getProperty("genre.getByBookId");
 
         List<Genre> books = template.query(sql_query, genreMapper, id);
+
+        checkIfCollectionIsNull(books);
+
         if (books.isEmpty()) {
             log.debug(String.format("Dont find any genre by bookID '%d'", id));
             return Collections.emptyList();
         } else {
             log.debug(String.format("Find %d genre(s) by bookID '%d'", books.size(), id));
             return books;
+        }
+    }
+
+    private void checkIfCollectionIsNull(Collection<Genre> collection) {
+        if (collection == null) {
+            // unreachable, but who knows (:
+            log.error("Get `null` reference from jdbcTemplate");
+            throw new DataBaseSQLException("Get `null` reference from jdbcTemplate");
+        }
+    }
+
+    private PreparedStatementCreator creator(String sql, Genre genre) throws SQLException {
+
+        PreparedStatementCreatorFactory factory = new PreparedStatementCreatorFactory(sql);
+        factory.setReturnGeneratedKeys(true);
+        factory.addParameter(new SqlParameter(Types.VARCHAR));
+
+        return factory.newPreparedStatementCreator(Collections.singletonList(genre.getName()));
+    }
+
+    private Integer retrieveId(KeyHolder holder) {
+
+        if (holder.getKeys() != null && holder.getKeys().size() > 0) {
+            return (Integer) holder.getKeys().get("genre_id");
+        } else {
+            return holder.getKey().intValue();
         }
     }
 }
